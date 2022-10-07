d9f0c16ad03c7bf3ce4dd4b8a7c6facf6c8f5423
Larry McCartin <larry.mccartin@das.oregon.gov>
2022-10-07 14:54:51 -0700

    Set the timestamp

--- main.js
+++ main.js
@@ -33,19 +33,6 @@ exec(`git rev-list --tags=${filter} --max-count=1`, (err, rev, stderr) => {
                 process.exit(0);
             }
             process.exit(1);
-        } else {
-            if(rev.length == 0) {
-                console.log('\x1b[33m%s\x1b[0m', 'Did not find matching tag');
-                if (process.env.INPUT_FALLBACK) {
-                    console.log('\x1b[33m%s\x1b[0m', 'Will use fallback tag and HEAD commit');
-                    console.log(`::set-output name=tag::${process.env.INPUT_FALLBACK}`);
-                    console.log(`::set-output name=timestamp::${timestamp}`);
-                    process.exit(0);
-                } else {
-                    console.log('\x1b[33m%s\x1b[0m', 'No fallback tag specified');
-                    process.exit(1);
-                }
-            }    
         }
 
         tag = tag.trim()
@@ -59,11 +46,25 @@ exec(`git rev-list --tags=${filter} --max-count=1`, (err, rev, stderr) => {
 
             timestamp = timestamp.trim()
 
-            console.log('\x1b[32m%s\x1b[0m', `Found tag: ${tag}`);
-            console.log('\x1b[32m%s\x1b[0m', `Found timestamp: ${timestamp}`);
-            console.log(`::set-output name=tag::${tag}`);
-            console.log(`::set-output name=timestamp::${timestamp}`);
-            process.exit(0);
+            if(rev.length == 0) {
+                // didn't really find the tag        
+                console.log('\x1b[33m%s\x1b[0m', 'Did not find matching tag');
+                if (process.env.INPUT_FALLBACK) {
+                    console.log('\x1b[33m%s\x1b[0m', 'Will use fallback tag and HEAD commit');
+                    console.log(`::set-output name=tag::${process.env.INPUT_FALLBACK}`);
+                    console.log(`::set-output name=timestamp::${timestamp}`);
+                    process.exit(0);
+                } else {
+                    console.log('\x1b[33m%s\x1b[0m', 'No fallback tag specified');
+                    process.exit(1);
+                }
+            } else {
+                console.log('\x1b[32m%s\x1b[0m', `Found tag: ${tag}`);
+                console.log('\x1b[32m%s\x1b[0m', `Found timestamp: ${timestamp}`);
+                console.log(`::set-output name=tag::${tag}`);
+                console.log(`::set-output name=timestamp::${timestamp}`);
+                process.exit(0);
+            }
         });
     });
 });
